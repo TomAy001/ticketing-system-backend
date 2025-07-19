ad577c9b1e020f551633e3d372a64ade125a1536
Tomide <matthieujackie@gmail.com>
2025-07-19 22:44:34 +0100

    14th commit

--- static/js/main.js
+++ static/js/main.js
@@ -289,17 +289,17 @@ document.addEventListener('keydown', function(e) {
 });
 
 // Progressive Web App support
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/static/js/sw.js')
-            .then(function(registration) {
-                console.log('ServiceWorker registration successful');
-            })
-            .catch(function(error) {
-                console.log('ServiceWorker registration failed');
-            });
-    });
-}
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function() {
+//         navigator.serviceWorker.register('/static/js/sw.js')
+//             .then(function(registration) {
+//                 console.log('ServiceWorker registration successful');
+//             })
+//             .catch(function(error) {
+//                 console.log('ServiceWorker registration failed');
+//             });
+//     });
+// }
 
 // Performance monitoring
 function measurePageLoad() {
