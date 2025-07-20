61a465c5c1ed8f925844956d5c015a0e218f9b80
Tomide <matthieujackie@gmail.com>
2025-07-20 16:06:47 +0100

    18th commit

--- ict_ticketing/settings.py
+++ ict_ticketing/settings.py
@@ -12,6 +12,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['ticketing-system-backend-d96m.onrender.com', 'localhost', '127.0.0.1', '192.168.0.199']
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://ticketing-system-backend-d96m.onrender.com"
+]
+
 # Application definition
 INSTALLED_APPS = [
     'channels',
